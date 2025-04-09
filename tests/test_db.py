a34b4b31831155f90acf2a8da664aee271b193a0
GuiVianaDev <guilhermevianna@gmail.com>
2025-04-09 09:05:20 -0300

    feat: add database, test with database and migrations

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,17 +1,27 @@
+from dataclasses import asdict
+
 from sqlalchemy import select
 
 from app.models.models import Account
 
 
-def test_create_account(session):
-    new_account = Account(
-        username='Junin', password='secret', email='test@test'
-    )
-    session.add(new_account)
-    session.commit()
+def test_create_account(session, mock_db_time):
+    with mock_db_time(model=Account) as time:
+        new_account = Account(
+            username='Junin', password='secret', email='test@test'
+        )
+        session.add(new_account)
+        session.commit()
 
     account = session.scalar(
         select(Account).where(Account.username == 'Junin')
     )
 
-    assert account.username == 'Junin'
+    assert asdict(account) == {
+        'id': 1,
+        'username': 'Junin',
+        'password': 'secret',
+        'email': 'test@test',
+        'created_at': time,
+        'updated_at': time,
+    }
