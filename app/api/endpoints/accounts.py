5501d85e40641e52971ac3afbc737b7984b457ea
GuiVianaDev <guilhermevianna@gmail.com>
2025-04-09 10:00:53 -0300

    feat: add bd in endpoint accounts

--- app/api/endpoints/accounts.py
+++ app/api/endpoints/accounts.py
@@ -1,11 +1,46 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter
-
+from fastapi import APIRouter, HTTPException, Depends
+from app.database import get_session
+from sqlalchemy import select
+from app.models.models import Account
+from sqlalchemy.orm import Session
 from app.schemas.account_schema import AccountPublic, AccountSchema
+
 
 router = APIRouter(prefix='/accounts', tags=['accounts'])
 
 
 @router.post('/', status_code=HTTPStatus.CREATED, response_model=AccountPublic)
-async def create_account(account: AccountSchema): ...
+async def create_account(
+    account: AccountSchema, session: Session = Depends(get_session)
+): 
+    db_account = session.scalar(
+        select(Account).where(
+            (Account.username == account.username) | (Account.email == account.email)
+        )
+    )
+    if db_account:
+        if db_account.username == account.username:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail='Username already exists',
+            )
+        elif db_account.email == account.email:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail='Email already exists',
+            )
+    
+    db_account = Account(
+        username=account.username,
+        password=account.password,
+        email=account.email
+    )
+    session.add(db_account)
+    session.commit()
+    session.refresh(db_account)
+
+    return db_account
+        
+        
